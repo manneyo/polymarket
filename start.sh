23db7d625d27a9ec2b4596d21469e9dc01cdea87
Emmanuel Ogah <eogah@hotmail.com>
2026-02-15 17:54:19 -0500

    Add start script for Railway

---
+++ start.sh
@@ -0,0 +1,14 @@
+Earnings
+
+Fubo sell on earnings
+GTIM good times,  they are always looking to sell off due to sell off of bag holders
+
+fsr stock 7-10 point move
+
+upside guidance - open 08-11 
+                     - looks like upside guidance and then button curl up, sono
+					 
+					 Macy with upside guidance is moving up
+
+
+When the amba does well in earnings it tends to fly 
